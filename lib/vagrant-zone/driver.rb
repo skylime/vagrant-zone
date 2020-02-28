d43200a2f419afdc3bb76402515e9e4916f37c64
Thomas Merkel <tm@core.io>
2020-02-28 13:02:54 +0100

    replace ssh key on startup

--- lib/vagrant-zone/driver.rb
+++ lib/vagrant-zone/driver.rb
@@ -128,9 +128,10 @@ module VagrantPlugins
 
 			def setup(machine, ui)
 				zlogin(machine, %('echo nameserver 1.1.1.1 >> /etc/resolv.conf'))
-				return if user_exists?(machine)
-				zlogin(machine, "useradd -m -s /bin/bash -U vagrant")
-				zlogin(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers.d/vagrant'))
+				if not user_exists?(machine)
+					zlogin(machine, "useradd -m -s /bin/bash -U vagrant")
+				end
+				zlogin(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant'))
 				zlogin(machine, "mkdir -p /home/vagrant/.ssh")
 				zlogin(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
 				zlogin(machine, "chown -R vagrant:vagrant /home/vagrant/.ssh")
