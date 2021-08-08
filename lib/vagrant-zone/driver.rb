82cda7e96dc71d33b3ee0f7035552e2b85e6fe7b
Mark <mark.gilbert@m4kr.net>
2021-08-08 09:16:52 -0500

    Update driver.rb

--- lib/vagrant-zone/driver.rb
+++ lib/vagrant-zone/driver.rb
@@ -177,6 +177,11 @@ module VagrantPlugins
 							set value="on,wait"
 						end
 						add attr
+							set name=console
+							set type=string
+							set value=socket,/tmp/vm.console
+						end
+						add attr
 							set name="acpi"
 							set type="string"
 							set value="off"
