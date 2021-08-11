41a86a84a7431298080792c09710847dde8456f1
Mark <mark.gilbert@m4kr.net>
2021-08-11 01:27:36 -0500

    Update driver.rb

--- lib/vagrant-zone/driver.rb
+++ lib/vagrant-zone/driver.rb
@@ -238,15 +238,13 @@ module VagrantPlugins
 				responses = []
 				sleep setup_wait
 				PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read,zlogin_write,pid|
-				        zlogin_read.expect(/\n/) { |msg| zlogin_write.printf("#{cmd} && echo \"Output: $?\"\n") }
+				        zlogin_read.expect(/\n/) { |msg| zlogin_write.printf("#{cmd} ; echo \"Output: $?\"\n") }
+					zlogin_write.printf("echo \"\"\n") 
 				        loop do
 				                zlogin_read.expect(/\r\n/) { |line|  responses.push line}
 				                puts responses[-1]
 				                if responses[-1].include? "Output: 0\r\n"
 				                        break
-						else
-							raise "Could not access the Bhyve zlogin console"
-				                end
 				        end
 				end
 			end
