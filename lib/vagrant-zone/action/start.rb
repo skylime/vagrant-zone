97441085252e7666c7d9b31132fc4039299814eb
Mark <mark.gilbert@m4kr.net>
2021-08-13 17:21:14 -0500

    Update start.rb

--- lib/vagrant-zone/action/start.rb
+++ lib/vagrant-zone/action/start.rb
@@ -5,7 +5,7 @@ module VagrantPlugins
 		module Action
 			class Start
 				def initialize(app, env)
-					@logger = Log4r::Logger.new("vagrant_zone::action::import")
+					@logger = Log4r::Logger.new("vagrant_zones::action::import")
 					@app = app
 				end
 
