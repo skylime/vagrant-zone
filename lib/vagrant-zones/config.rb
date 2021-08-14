e59283a16bb597d29bd82849eb4d1f43bf1f9a97
Mark <mark.gilbert@m4kr.net>
2021-08-14 12:23:31 -0500

    Update config.rb

--- lib/vagrant-zones/config.rb
+++ lib/vagrant-zones/config.rb
@@ -60,17 +60,19 @@ module VagrantPlugins
 				@hostbridge   			= 'i440fx'
 				@acpi 				= 'on'
 				@firmware_type 			= "compatability"
+				@firmware 			= UNSET_VALUE
 				@dhcp   			= false
 				@setup_wait  			= 30
 				@clean_shutdown_time  		= 300
 				@vmtype   			= 'production'
+				@vm_type   			= UNSET_VALUE
 				@partition_id  			= '0000'
 				@sshport  			= '22'
 				@rdpport  			= '3389'
 				@vagrant_user   		= 'vagrant'
 				@vagrant_user_pass  		= 'vagrant'
 				@vagrant_user_private_key_path  =  UNSET_VALUE
-				@cdrom_path			=  'none'
+				@cdrom_path			= "none"
 				@vagrant_user_key  		= 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key'
 
 				
