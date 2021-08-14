0c11f6fbaa9de6b62d47f28e68fa30bb7a52fd1b
Mark <mark.gilbert@m4kr.net>
2021-08-13 20:41:41 -0500

    Update config.rb

--- lib/vagrant-zones/config.rb
+++ lib/vagrant-zones/config.rb
@@ -17,6 +17,7 @@ module VagrantPlugins
 			attr_accessor :vagrant_user_private_key_path
 			attr_accessor :setup_wait
 			attr_accessor :dhcp
+			attr_accessor :vagrant_user_pass
 			
 			def initialize
 				# pkgsrc, lx, bhyve, kvm, illumos
@@ -32,6 +33,7 @@ module VagrantPlugins
 				@dhcp   = false
 				@setup_wait  = 120
 				@vagrant_user   = 'vagrant'
+				@vagrant_user_pass   = 'vagrant'
 				@vagrant_user_private_key_path   =  UNSET_VALUE
 				@vagrant_user_key   = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key'
 			end
