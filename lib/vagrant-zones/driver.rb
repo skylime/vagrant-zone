bfa2bc6579aa5ccf1f288ec682350606a74ce466
Mark <mark.gilbert@m4kr.net>
2021-08-14 04:05:35 -0500

    Update driver.rb

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -12,9 +12,16 @@ require 'vagrant-zones/util/timer'
 
 module VagrantPlugins
 	module ProviderZone
+		
+		## Convert Subnet Mask into CIDR Notation
+		IPAddr.class_eval
+		  def to_cidr
+		    "/" + self.to_i.to_s(2).count("1")
+		  end
+		end
+		
 		class Driver
 			attr_accessor :executor
-
 			def initialize(machine)
 				@logger = Log4r::Logger.new("vagrant_zones::driver")
 				@machine = machine
@@ -56,13 +63,18 @@ module VagrantPlugins
 			def get_ip_address(machine)
 				config = machine.provider_config
 				dhcpenabled = config.dhcp
-				machine.config.vm.networks.each do |_type, opts|
-					if _type.to_s == "public_network"
-						ip        = opts[:ip].to_s
-						network   = NetAddr.parse_net(opts[:ip].to_s + '/' + opts[:netmask].to_s)
-						defrouter = opts[:gateway]
-						return nil if ip.length == 0
-						return ip.gsub /\t/, ''
+				if dhcpenabled
+					raise "==> #{machine.} ==> DHCP is not yet Configured for use"
+				else
+					machine.config.vm.networks.each_with_index do |_type, opts, index|
+						index + 1
+						if _type.to_s == "public_network"
+							ip        = opts[:ip].to_s
+							network   = NetAddr.parse_net(opts[:ip].to_s + '/' + opts[:netmask].to_s)
+							defrouter = opts[:gateway]
+							return nil if ip.length == 0
+							return ip.gsub /\t/, ''
+						end
 					end
 				end
 			end
@@ -78,15 +90,26 @@ module VagrantPlugins
 				if config.brand == 'bhyve'
 					execute(false, "#{@pfexec} zoneadm -z #{name} install")
 				end
+				if config.brand == 'kvm'
+					execute(false, "#{@pfexec} zoneadm -z #{name} install")
+				end
+				if config.brand == 'illumos'
+					execute(false, "#{@pfexec} zoneadm -z #{name} install")
+				end
 			end
-
+			
+			## Boot the Machine
 			def boot(machine, ui)
 				name = @machine.name
 				execute(false, "#{@pfexec} zoneadm -z #{name} boot")
 			end
-
-			def create_vnic(machine, ui)
-				machine.config.vm.networks.each do |_type, opts|
+			
+			## Create Network Interfaces
+			def vnic(machine, ui, state)
+				config = machine.provider_config
+				machine.config.vm.networks.each_with_index do |_type, opts, index|
+					index + 1
+					nic_number = index
 					if _type.to_s == "public_network"
 						link = opts[:bridge]
 						mac  = 'auto'
@@ -94,46 +117,50 @@ module VagrantPlugins
 						if !opts[:mac].nil?
 							mac  = opts[:mac]
 						end
-						if !opts[:vlan].nil?
-							vlan =  opts[:vlan]
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} -v #{vlan} #{machine.name}0")
+						if !opts[:type].nil?
+							nictype  = opts[:nictype]
+						end
+
+						case nictype
+						when /external/
+						  nic_type = "e"
+						when /internal/
+						  nic_type = "i"
+						when /carp/
+						  nic_type = "c"
+						when /management/
+						  nic_type = "m"
+						when /host/
+						  nic_type = "h"
 						else
-							
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} #{machine.name}0")
+						  nic_type = "e"
+						end
+						if state == "create"
+							if !opts[:vlan].nil?
+								vlan =  opts[:vlan]
+								execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} -v #{vlan} vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id}")
+							else
+								execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id}")
+							end		
+						elsif state == "delete"
+							vnic_configured = execute(false, "#{@pfexec} dladm show-vnic | grep vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id} | awk '{ print $1 }' ")
+							if vnic_configured == "vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id}"
+								execute(false, "#{@pfexec} dladm delete-vnic vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id}")
+							end
+						elsif state == "config"
+							nic_attr = %{
+								add net
+									set physical=vnic#{nic_type}#{config.vm_type}#{nic_number}-#{config.partition_id}
+								end
+							}
+							additional_nics_data = %{
+								#{nic_attr}
+							}
+							File.open('zone_config', 'a') do |f|
+								f.puts additional_nics_data
+							end
 						end
 					end
-					if _type.to_s == "private_network"
-						link = opts[:bridge]
-						mac  = 'auto'
-						vlan = 1
-						if !opts[:mac].nil?
-							mac  = opts[:mac]
-						end
-						if !opts[:vlan].nil?
-							vlan =  opts[:vlan]
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} -v #{vlan} #{machine.name}0")
-						else
-							
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} #{machine.name}0")
-						end
-					end
-					if _type.to_s == "ha_network"
-						link = opts[:bridge]
-						mac  = 'auto'
-						vlan = 1
-						if !opts[:mac].nil?
-							mac  = opts[:mac]
-						end
-						if !opts[:vlan].nil?
-							vlan =  opts[:vlan]
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} -v #{vlan} #{machine.name}0")
-						else
-							
-							execute(false, "#{@pfexec} dladm create-vnic -l #{link} -m #{mac} #{machine.name}0")
-						end
-					end
-				
-					
 				end
 			end
 
@@ -154,7 +181,6 @@ module VagrantPlugins
 					disk1size = config.disk1_size.to_s
 					execute(false, "#{@pfexec} zfs create -V #{disk1size} #{disk1path}")
 				end
-
 			end
 
 			def delete_dataset(machine, ui)
@@ -163,12 +189,13 @@ module VagrantPlugins
 			end
 
 			def zonecfg(machine, ui)
+				## Seperate commands out to indvidual functions like Network, Dataset, and Emergency Console
 				config = machine.provider_config
-				lofs_current_dir = Dir.pwd
-
+				config.shared_dir = Dir.pwd
 				attr = ''
 				if config.brand == 'lx'
-					machine.config.vm.networks.each do |_type, opts|
+					machine.config.vm.networks.each_with_index do |_type, opts, index|
+						index + 1
 						if _type.to_s == "public_network"
 							@ip        = opts[:ip].to_s
 							@network   = NetAddr.parse_net(opts[:ip].to_s + '/' + opts[:netmask].to_s)
@@ -201,28 +228,17 @@ module VagrantPlugins
 					}
 				end
 				if config.brand == 'bhyve'
+					## General Configuration
 					attr = %{
+						create
+						set zonepath=#{config.zonepath}/path
+						set brand=#{config.brand}
+						set autoboot=#{config.autoboot}
 						set ip-type=exclusive
-						add device
-							set match=/dev/zvol/rdsk#{config.zonepath}/boot
-						end
-						add attr
-							set name=bootrom
-							set type=string
-							set value=BHYVE_RELEASE_CSM
-						end
-						add net
-							set physical=#{machine.name}0
-						end
-						add attr
-							set name=bootdisk
-							set type=string
-							set value=#{config.zonepath.delete_prefix("/")}/boot
-						end
 						add attr
 							set name="acpi"
 							set type="string"
-							set value="off"
+							set value="#{config.acpi}"
 						end
 						add attr
 							set name="vcpus"
@@ -234,35 +250,90 @@ module VagrantPlugins
 							set type="string"
 							set value=#{config.memory}
 						end
+						add attr
+							set name=bootrom
+							set type=string
+							set value=#{config.firmware}
+						end
+						add device
+							set match=/dev/zvol/rdsk#{config.zonepath}/boot
+						end
+						add attr
+							set name=bootdisk
+							set type=string
+							set value=#{config.zonepath.delete_prefix("/")}/boot
+						end
 					}
-				end
 
+				end
 				data = %{
-					create
-					set zonepath=#{config.zonepath}/path
-					set brand=#{config.brand}
-					set autoboot=true
 					#{attr}
-					add fs
-						set dir=/vagrant
-						set special=#{lofs_current_dir}
-						set type=lofs
-					end
-					exit
 				}
 				File.open('zone_config', 'w') do |f|
 					f.puts data
 				end
+				
+				## Shared Disk Configurations
+				if config.shared_disk_enabled
+					shared_disk_attr = %{
+						add fs
+							set dir=/vagrant
+							set special=#{lofs_current_dir}
+							set type=lofs
+						end
+					}
+					shared_disk_data = %{
+						#{shared_disk_attr}
+					}
+				
+					File.open('zone_config', 'a') do |f|
+						f.puts shared_disk_data
+					end
+				end
+				
+				
+				## Additional Disk Configurations
+				additional_disk_attr = %{
+					add device
+						set match=/dev/zvol/rdsk#{config.zonepath}/disk1
+					end
+					add attr
+						set name=disk
+						set type=string
+						set value=#{config.zonepath.delete_prefix("/")}/disk1
+					end
+				}
+				additional_disks_data = %{
+					#{additional_disk_attr}
+				}
+				File.open('zone_config', 'a') do |f|
+					f.puts additional_disks_data
+				end
+				
+				## Nic Configurations
+				state = "config"
+				@driver.vnic(@machine, env[:ui], state)
+
+
+				## Write out Config
+				exit = %{
+					exit
+				}
+				File.open('zone_config', 'a') do |f|
+					f.puts exit
+				end
+				
+				## Export config to zonecfg
 				execute(false, "cat zone_config | #{@pfexec} zonecfg -z #{machine.name}")
 			end
 
-			def check_bhyve_support(machine, ui)
+			def check_zone_support(machine, ui)
 				config = machine.provider_config
 				box  = @machine.data_dir.to_s + '/' + @machine.config.vm.box
 				name = @machine.name
 
 				if config.brand == 'lx'
-					execute(false, "#{@pfexec} zoneadm -z #{name} install -s #{box}")
+					return
 				end
 				if config.brand == 'bhyve'			
 					## Check for  bhhwcompat
@@ -273,10 +344,8 @@ module VagrantPlugins
 						raise Errors::MissingCompatCheckTool if result == 0
 					end
 					
-					
-					
 					# Check whether OmniOS version is lower than r30
-					result = execute(true, "VER=$(cat /etc/release | head -n 1 | cut -d' ' -f5 |  cut -c 2-); test $VER -ge 1510380")
+					result = execute(true, "/usr/bin/bash -c \"RELEASE=1510380;VER=$(cat /etc/release | head -n 1 | cut -d' ' -f5 |  cut -c 2-); if (($VER -gt $RELEASE)); then exit 0; else exit 1; fi\"")
 					puts ""
 					puts result
 					puts ""
@@ -286,8 +355,6 @@ module VagrantPlugins
 					puts ""
 					puts ""
 					raise Errors::SystemVersionIsTooLow if result == 0
-					
-				
 	
 					# Check Bhyve compatability
 					result = execute(false, "#{@pfexec} bhhwcompat -s")
@@ -302,7 +369,8 @@ module VagrantPlugins
 				
 				puts "==> #{name}: Waiting for the Machine to boot..."
 				waitforboot(machine)
-
+				
+				## Check if already setup and skip the following
 				
 				if insert_key
 					zlogin(machine, "echo #{vagrant_user_key} > \/home\/#{config.vagrant_user}\/.ssh\/authorized_keys")
@@ -310,32 +378,39 @@ module VagrantPlugins
 					zlogin(machine, "chmod 600 \/home\/#{config.vagrant_user}\/.ssh\/authorized_keys")
 				end
 				
-				
-				machine.config.vm.networks.each do |_type, opts|
+				machine.config.vm.networks.each_with_index do |_type, opts, index|
+					index + 1
 					if _type.to_s == "public_network"
-						ip        = opts[:ip].to_s
-						network   = NetAddr.parse_net(opts[:ip].to_s + '/' + opts[:netmask].to_s)
-						defrouter = opts[:gateway]
-						
-						
+						ip        	= opts[:ip].to_s
+						netmask 	= IPAddr.new(opts[:netmask]).to_cidr
+						defrouter 	= opts[:gateway]
+						if !opts[:nameserver1].nil?
+							nameserver1  = opts[:nameserver1].to_s
+						else
+							nameserver1  = "1.1.1.1"
+						end
+						if !opts[:nameserver2].nil?
+							nameserver2  = opts[:nameserver2].to_s
+						else
+							nameserver2  = "1.0.0.1"
+						end
 						## Remove old installer netplan config
 						zlogin(machine, "rm -rf /etc/netplan/00-installer-config.yaml")
 						
 						## Create new netplan config
 						zlogin(machine, "touch /etc/netplan/00-installer-config.yaml")
 						zlogin(machine, 'echo "network:" > /etc/netplan/00-installer-config.yaml')
+						zlogin(machine, 'sed -i "$ a \  version: 2" /etc/netplan/00-installer-config.yaml')
 						zlogin(machine, 'sed -i "$ a \  ethernets:" /etc/netplan/00-installer-config.yaml')
 						zlogin(machine, 'APT=$(ifconfig -s -a | grep -v lo | tail -1 | awk \'{ print $1 }\') &&  sed -i "$ a \    $APT:" /etc/netplan/00-installer-config.yaml')
 						zlogin(machine, 'sed -i "$ a \      dhcp-identifier: mac" /etc/netplan/00-installer-config.yaml')
 						zlogin(machine, 'sed -i "$ a \      dhcp4: no" /etc/netplan/00-installer-config.yaml')
+						zlogin(machine, 'sed -i "$ a \      dhcp6: no" /etc/netplan/00-installer-config.yaml')
 						zlogin(machine, 'sed -i "$ a \      nameservers:" /etc/netplan/00-installer-config.yaml')
-						zlogin(machine, 'sed -i "$ a \        addresses:" /etc/netplan/00-installer-config.yaml')
-						zlogin(machine, 'sed -i "$ a \        - 1.1.1.1" /etc/netplan/00-installer-config.yaml')
-						zlogin(machine, 'sed -i "$ a \        - 8.8.8.8" /etc/netplan/00-installer-config.yaml')
-						zlogin(machine, 'sed -i "$ a \      addresses:" /etc/netplan/00-installer-config.yaml')
-						zlogin(machine, "sed -i '$ a \\        - #{ip}\/24' /etc/netplan/00-installer-config.yaml")
+						zlogin(machine, "sed -i '$ a \\        addresses: [#{nameserver1} , #{nameserver2}]' /etc/netplan/00-installer-config.yaml")
+						zlogin(machine, "sed -i '$ a \\      addresses: [#{ip}\/#{netmask}]' /etc/netplan/00-installer-config.yaml")
 						zlogin(machine, "sed -i '$ a \\      gateway4: #{defrouter}' /etc/netplan/00-installer-config.yaml")
-						zlogin(machine, 'sed -i "$ a \  version: 2" /etc/netplan/00-installer-config.yaml')
+						
 						
 						## Apply the Configuration
 						puts "==> #{name}: Applying the network configuration"
@@ -349,19 +424,18 @@ module VagrantPlugins
 			def waitforboot(machine)
 				name = @machine.name
 				config = machine.provider_config
-				setup_wait = config.setup_wait
 				responses = []
-				subresponses = []
 				PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read,zlogin_write,pid|
 				        if zlogin_read.expect(/Last login: /)
 						puts "==> #{name}: Machine Booted, Running Setup"
 						sleep 5
-						Timeout.timeout(setup_wait) do
+						Timeout.timeout(config.setup_wait) do
 							loop do
 				        		       	zlogin_read.expect(/\n/) { |line|  responses.push line}
 								if responses[-1].to_s.match(/:~#/)
 									break
 								elsif responses[-1].to_s.match(/login: /)
+									## Code to try to login with username and password
 									puts "==> #{name}: Could not login as Root, Check if Root Autologin Works"
 								end
 							end
@@ -384,7 +458,7 @@ module VagrantPlugins
 							if responses[-1].to_s.match(/Error Code: 0/)
 						        	break
 							elsif responses[-1].to_s.match(/Error Code: \b(?![0]\b)\d{1,4}\b/)
-						        	raise "Command: #{cmd} Failed with: responses[-1]"
+						        	raise "==> #{name}: \nCommand: \n ==> #{cmd} \nFailed with: \n responses[-1]"
 							elsif responses[-1].nil?
 						                break
 							end
@@ -395,21 +469,18 @@ module VagrantPlugins
 			end
 
 			def user(machine)
-				name = @machine.name
 				config = machine.provider_config
 				user = config.vagrant_user
 				return user
 			end
 			
 			def userprivatekeypath(machine)
-				name = @machine.name
 				config = machine.provider_config
 				userkey = config.vagrant_user_private_key_path.to_s
 				return userkey
 			end
 
 			def vagrantuserpass(machine)
-				name = @machine.name
 				config = machine.provider_config
 				vagrantuserpass = config.vagrant_user_pass.to_s
 				return vagrantuserpass
@@ -419,36 +490,53 @@ module VagrantPlugins
 				name = @machine.name
 				vm_state = execute(false, "#{@pfexec} zoneadm -z #{name} list -p | awk -F: '{ print $3 }'")
 				vm_configured = execute(false, "#{@pfexec} zoneadm list -i | grep  #{name} || true")
-					if vm_state == "running"
-						execute(false, "#{@pfexec} zoneadm -z #{name} halt")
+				if vm_state == "running"
+					begin
+					 status = Timeout::timeout(config.clean_shutdown_time) {
+						execute(false, "#{@pfexec} zoneadm -z #{name} shutdown")
+					 }
+					rescue Timeout::Error
+  						puts "==> #{name}: VM failed to Shutdown in alloted time #{config.clean_shutdown_time}"
+						begin halt_status = Timeout::timeout(60) {
+							execute(false, "#{@pfexec} zoneadm -z #{name} halt")
+						}
+						rescue Timeout::Error
+							raise "==> #{name}: VM failed to halt in alloted time 60 after waiting to shutdown for #{config.clean_shutdown_time}"
+						end
 					end
+					
+				end
 			end
 
 			def destroy(machine, id)
 				name = @machine.name
 				
-				vnic_configured = execute(false, "#{@pfexec} dladm show-vnic | grep #{name}0 | awk '{ print $1 }' ")
+				## Ensure machine is halted
+				execute(false, "#{@pfexec} zoneadm -z #{name} halt")
+				
+				## Check if it has a presence in zoneadm and if no presence in zoneadm destroy zonecfg
 				vm_configured = execute(false, "#{@pfexec} zoneadm list -i | grep  #{name} || true")
-				if vm_configured == name
-					vm_state = execute(false, "#{@pfexec} zoneadm -z #{name} list -p | awk -F: '{ print $3 }'")
-					if vm_state == 'incomplete' || vm_state == 'configured' 
-						execute(false, "#{@pfexec} zoneadm -z #{name} uninstall -F")
-						execute(false, "#{@pfexec} zonecfg -z #{name} delete -F")
-						if vnic_configured == "#{name}0"
-							execute(false, "#{@pfexec} dladm delete-vnic #{name}0")
-						end
-					elsif vm_state == "installed"
-						execute(false, "#{@pfexec} zoneadm -z #{name} uninstall -F")
-						execute(false, "#{@pfexec} zonecfg -z #{name} delete -F")
-						if vnic_configured == "#{name}0"
-							execute(false, "#{@pfexec} dladm delete-vnic #{name}0")
-						end
-					end
-				else
+				if vm_configured != name
 					execute(false, "#{@pfexec} zonecfg -z #{name} delete -F")
-					if vnic_configured == "#{name}0"
-							execute(false, "#{@pfexec} dladm delete-vnic #{name}0")
-					end
+				end
+				
+				## Check state in zoneadm
+				vm_state = execute(false, "#{@pfexec} zoneadm -z #{name} list -p | awk -F: '{ print $3 }'")
+				
+				## If state is seen, uninstall from zoneadm and destroy from zonecfg
+				if vm_state == 'incomplete' || vm_state == 'configured' || vm_state ==  "installed"
+					execute(false, "#{@pfexec} zoneadm -z #{name} uninstall -F")
+					execute(false, "#{@pfexec} zonecfg -z #{name} delete -F")
+				end
+
+				### Nic Configurations
+				state = "delete"
+				@driver.vnic(@machine, env[:ui], state)
+				
+				
+				### Check State of additional Disks
+				#disks_configured = execute(false, "#{@pfexec}  zfs list ")
+
 				end
 			end
 		end
