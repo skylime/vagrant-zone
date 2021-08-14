fd0d29318bbfea4818afb7dbe9ffacabcc01b4c3
Mark <mark.gilbert@m4kr.net>
2021-08-14 11:05:38 -0500

    Update config.rb

--- lib/vagrant-zones/config.rb
+++ lib/vagrant-zones/config.rb
@@ -33,6 +33,7 @@ module VagrantPlugins
 			attr_accessor :hostbridge
 			attr_accessor :sshport
 			attr_accessor :rdpport
+			attr_accessor :cdrom_path
 			
 			
 			
@@ -45,7 +46,7 @@ module VagrantPlugins
 				@zonepath 			= UNSET_VALUE
 				@zonepathsize 			= UNSET_VALUE
 				@disk1pathsize 			= UNSET_VALUE
-				@disk1size 			= UNSET_VALUE
+				@disk1path 			= UNSET_VALUE
 				@shared_dir			= Dir.pwd
 				@os_type			= 'generic'
 				@shared_disk_enabled		= true
@@ -68,6 +69,7 @@ module VagrantPlugins
 				@vagrant_user   		= 'vagrant'
 				@vagrant_user_pass  		= 'vagrant'
 				@vagrant_user_private_key_path  =  UNSET_VALUE
+				@cdrom_path			=  UNSET_VALUE
 				@vagrant_user_key  		= 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key'
 
 				
