f1b4cff810b1b84544e53c2402a88e55a3c93fbd
Mark <mark.gilbert@m4kr.net>
2021-08-17 00:01:49 -0500

    Update driver.rb

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -263,16 +263,14 @@ end							}
       nameservers:
         addresses: [#{nameserver1} , #{nameserver2}]							}
 													##Command to Write out Config 
-													resp=[]
 													zlogin_write.printf("echo '#{netplan}' > /etc/netplan/#{vnic_name}.yaml; echo \"Exit Code: $?\"\n")
 													Timeout.timeout(30) do
 														loop do
-															zlogin_read.expect(/\r\n/) { |line|  resp.push line}
-															if resp[-1].to_s.match(/Exit Code: 0/)
+															if responses[-1].to_s.match(/Exit Code: 0/)
 																break
-															elsif resp[-1].to_s.match(/Exit Code: \b(?![0]\b)\d{1,4}\b/)
+															elsif responses[-1].to_s.match(/Exit Code: \b(?![0]\b)\d{1,4}\b/)
 																raise "==> #{name}: \nCommand: \n ==> #{cmd} \nFailed with: \n #{resp[-1]}"
-															elsif resp[-1].nil?
+															elsif responses[-1].nil?
 															        break
 															end
 														end
