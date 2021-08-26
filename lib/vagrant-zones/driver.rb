ff7d1a1234924a04d1b5a96afd989e07958d4f41
Mark <mark.gilbert@m4kr.net>
2021-08-26 09:22:52 -0500

    type

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -385,7 +385,7 @@ end							}
 			def delete_dataset(machine, ui)
 				name = @machine.name
 				config = machine.provider_config
-				ui.info(I18n.t("vagrant_zones.destroy_dataset"),zp =  )
+				ui.info(I18n.t("vagrant_zones.destroy_dataset") )
 				ui.info(" -- Zone Prefix: #{config.zonepath.delete_prefix("/")}")
 				execute(false, "#{@pfexec} zfs destroy -r #{config.zonepath.delete_prefix("/")}")
 			end
