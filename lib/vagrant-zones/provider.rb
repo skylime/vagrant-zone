99aa1a2d6b30802a79219590d4e513a3a3fded8c
Mark <mark.gilbert@m4kr.net>
2021-08-26 07:14:07 -0500

    auto off test

--- lib/vagrant-zones/provider.rb
+++ lib/vagrant-zones/provider.rb
@@ -24,7 +24,7 @@ module VagrantPlugins
 				user = driver.user(@machine)
 				userkey = driver.userprivatekeypath(@machine).to_s
 				vagrantuserpassword = driver.vagrantuserpass(@machine).to_s
-				passwordauth = "yes" 
+				passwordauth = "no" 
 				return nil if !ip
 				portnumber = "22"
 				unless portnumber.to_s.nil? || portnumber.to_s == 0
