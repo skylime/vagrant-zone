73cff6f82056f6a05a664d17bad7f1b8a4a50377
Mark <mark.gilbert@m4kr.net>
2021-08-27 03:39:00 -0500

    output change

--- lib/vagrant-zones/action/wait_till_up.rb
+++ lib/vagrant-zones/action/wait_till_up.rb
@@ -31,8 +31,7 @@ module VagrantPlugins
 					end
 					# if interrupted above, just terminate immediately
 					return terminate(env) if env[:interrupted]
-					ui.info(I18n.t("vagrant_zones.ssh_ready"))
-					ui.info(" -- #{env[:metrics]['instance_ssh_time']} Seconds")
+					ui.info(I18n.t("vagrant_zones.ssh_ready")," -- #{env[:metrics]['instance_ssh_time']} Seconds")
 					
 					@app.call(env)
 				end
