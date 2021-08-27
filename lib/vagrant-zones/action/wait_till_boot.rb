7d75b26a159c534d6f6f9d742c2658fb426ab6d4
Mark <mark.gilbert@m4kr.net>
2021-08-27 05:23:51 -0500

    change waittillboot

--- lib/vagrant-zones/action/wait_till_boot.rb
+++ lib/vagrant-zones/action/wait_till_boot.rb
@@ -19,23 +19,11 @@ module VagrantPlugins
 					ui = env[:ui]
 					# Initialize metrics if they haven't been
 					env[:metrics] ||= {}
-
-
 					env[:metrics]['instance_boot_time'] = Util::Timer.time do
-						retryable(on: Errors::TimeoutError, tries: 1) do
-							# If we're interrupted don't worry about waiting
-							next if env[:interrupted]
-							loop do
-								break if env[:interrupted]
-								break if @driver.waitforboot(@machine, ui)
-							end
-						end
+						break if env[:interrupted]
+						break if @driver.waitforboot(@machine, ui)
 					end
 
-
-
-					
-					
                     return terminate(env) if env[:interrupted]
                     ui.info(I18n.t("vagrant_zones.boot_ready") + " in #{env[:metrics]['instance_boot_time']} Seconds")
                     @app.call(env)
