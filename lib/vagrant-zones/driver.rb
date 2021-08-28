42dac9925952649d499a262e60f7556b7c540660
Mark <mark.gilbert@m4kr.net>
2021-08-27 22:36:07 -0500

    UTF-8 it

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -676,7 +676,7 @@ end
 				config = machine.provider_config
 				responses = []
 				PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read,zlogin_write,pid|
-				    if zlogin_read.expect(/Last login: /)
+				    if zlogin_read.encode('UTF-8').expect(/Last login: /)
 						ui.info(I18n.t("vagrant_zones.booted_check_terminal_access"))
 						Timeout.timeout(config.setup_wait) do
 							loop do
