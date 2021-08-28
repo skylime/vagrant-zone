a4e0a01fd8148bc1352d84df7a8eafda76fe767a
Mark <mark.gilbert@m4kr.net>
2021-08-27 22:05:18 -0500

    update

--- lib/vagrant-zones/util/subprocess.rb
+++ lib/vagrant-zones/util/subprocess.rb
@@ -7,13 +7,11 @@ module VagrantPlugins
           Open3.popen3(cmd) do |_stdin, stdout, stderr, thread|
             # read each stream from a new thread
             { out: stdout, err: stderr }.each do |key, stream|
-                until (line = stream.gets).nil? do
-                  # yield the block depending on the stream
-                  if key == :out
-                    yield line, nil, thread if block_given?
-                  else
-                    yield nil, line, thread if block_given?
-                  end
+              stream.each_line do |line|
+                if key == :out
+                  yield line, nil, thread if block_given?
+                elsif key == :err
+                  yield nil, line, thread if block_given?
                 end
               end
             end
