eb77c1737bbf49ce42e4960f37aedd4f3b5aa5f9
Mark <mark.gilbert@m4kr.net>
2021-08-27 21:49:39 -0500

    remove parts

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -66,6 +66,7 @@ module VagrantPlugins
 						command = "#{@pfexec} pv -n #{env[:machine].box.directory.join('box.zss').to_s}  > #{datadir.to_s + '/box.zss'} "
 						Util::Subprocess.new command do |stdout, stderr, thread|
 							ui.report_progress(stderr, 100, show_parts=false)
+							ui.clear_line
 						  end
 						  ui.clear_line
 						
