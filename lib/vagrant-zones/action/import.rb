5c32d014aced86470d122c0fcf56a9c5ddbf1789
Mark <mark.gilbert@m4kr.net>
2021-08-27 20:45:56 -0500

    progress test?

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -65,9 +65,14 @@ module VagrantPlugins
 						@driver.execute(false, "#{@pfexec} pv #{env[:machine].box.directory.join('box.zss').to_s}  > #{datadir.to_s + '/box.zss'} ")
 
 						progressbar = ProgressBar.create( :format => "%a %b\u{15E7}%i %p%% %t", :progress_mark => ' ', :remainder_mark => "\u{FF65}", :starting_at => 10,:length => 100)
-						100.times { progressbar.increment; sleep 0.1 ;}
-						reset = "\r\033[K"
-						ui.info(reset, new_line: false)
+						
+						
+						
+						total = 100
+						progress = 0
+						100.times { ui.report_progress(progress, total); sleep 0.1 ; progress +=1 }
+						
+						ui.clear_line
 
 
 
