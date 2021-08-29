9aa55dd0c3858c8aa532fab962a1a1182a1f3c04
Mark <mark.gilbert@m4kr.net>
2021-08-29 14:16:06 -0500

    save

--- lib/vagrant-zones/action/list_zfs_snapshots.rb
+++ lib/vagrant-zones/action/list_zfs_snapshots.rb
@@ -1,19 +1,18 @@
 # coding: utf-8
 module VagrantPlugins
 	module ProviderZone
-      module Action
-        class ListSnapshots
-          def initialize(app, _env)
-            @app = app
-          end
-
-          def call(env)
-            if env[:machine].id
-              env[:ui].info (env[:machine].id)
-              env[:ui].info ("test")
-            end
-            @app.call(env)
+    module Action
+      class ListSnapshots
+        def initialize(app, _env)
+          @app = app
         end
+        def call(env)
+          if env[:machine].id
+            env[:ui].info (env[:machine].id)
+            env[:ui].info ("test")
+          end
+        @app.call(env)
       end
     end
   end
+end
