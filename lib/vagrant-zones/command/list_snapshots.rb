d1a67e6931a24c3fa15bf791d1f36d2fb091751d
Mark <mark.gilbert@m4kr.net>
2021-08-29 17:30:50 -0500

    s

--- lib/vagrant-zones/command/list_snapshots.rb
+++ lib/vagrant-zones/command/list_snapshots.rb
@@ -24,8 +24,9 @@ module VagrantPlugins
 
             snapshot = "none" if argv[0].nil?
             snapshot = argv[0] if !argv[0].nil?
+            puts snapshot
             with_target_vms(argv, provider: :zone) do |machine|
-              if !options[:snapshot].nil? && options[:snapshot] != 'none'
+              if !options[:snapshot].nil? && snapshot != 'none'
                 machine.action('list_zfs_snapshots', options[:snapshot] ) 
               end
             end
