238dbf12af933adfe5c6bca780f1fb1828585d28
Mark <mark.gilbert@m4kr.net>
2021-08-29 17:15:04 -0500

    s

--- lib/vagrant-zones/command/list_snapshots.rb
+++ lib/vagrant-zones/command/list_snapshots.rb
@@ -23,7 +23,7 @@ module VagrantPlugins
             options[:snapshot] = argv[0]
             puts
             puts
-            puts options[:snapshot]
+            puts  argv[0]
             with_target_vms(argv, provider: :zone) do |machine|
               machine.action('list_zfs_snapshots', options[:snapshot] )
             end
