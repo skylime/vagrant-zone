b88b49e99f0a4d803f63488b5580761008724c3b
Mark <mark.gilbert@m4kr.net>
2021-08-29 18:26:32 -0500

    s

--- lib/vagrant-zones/action/list_zfs_snapshots.rb
+++ lib/vagrant-zones/action/list_zfs_snapshots.rb
@@ -6,7 +6,7 @@ module VagrantPlugins
         def initialize(app, _env)
           @app = app
         end
-        def call(env)
+        def call(env, snapshot)
           puts env
           @machine = env[:machine]
           @driver  = @machine.provider.driver
