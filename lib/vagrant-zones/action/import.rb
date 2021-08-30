6d564f227b311cffcddbce2cb42b9f5c72ef1dbf
Mark <mark.gilbert@m4kr.net>
2021-08-29 23:21:24 -0500

    s

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -45,7 +45,7 @@ module VagrantPlugins
 						
 						
 						
-						uri = URI("https://#{@joyent_images_url}/#{image}/file")
+						uri = URI("#{@joyent_images_url}/#{image}/file")
 						puts uri
 
 						Net::HTTP.start(uri.host, uri.port,	:use_ssl => uri.scheme == 'https') do |http|
