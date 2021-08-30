e695e10284abbf2c55f11ddebc291e69c3dff79c
Mark <mark.gilbert@m4kr.net>
2021-08-29 23:43:31 -0500

    s

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -66,7 +66,7 @@ module VagrantPlugins
 										if ratelimit == 0
 											ratelimit += 1
 											status = "%.2f%%" % (amount_downloaded.to_f / file_size * 100)
-											ui.info("==> #{name}: Import "+  status, new_line: true)
+											ui.info("==> #{name}: Import "+  status.strip, new_line: true)
 											ui.report_progress(status, 100, false)
 											ui.clear_line()
 											if ratelimit == 2
