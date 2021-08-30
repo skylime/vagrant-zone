1bdebaad55cca227444073445b15215ee6755493
Mark <mark.gilbert@m4kr.net>
2021-08-29 22:33:39 -0500

    s

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -45,11 +45,12 @@ module VagrantPlugins
 						raise Vagrant::Errors::BoxNotFound if not check(image)
 
 						command = "#{@pfexec} curl --output #{datadir.to_s + '/' + image}  #{@joyent_images_url}/#{image}/file --progress-bar 2>&1 | tr $'\\r' $'\\n' | sed -r 's/[# ]+|%|=|-|O|.[0-9]//g;'"
-						puts "test"
+						
 						Util::Subprocess.new command do |stdout, stderr, thread|
+							ui.info("==> #{name}: Import ", new_line: false)
 							ui.rewriting do |ui|
 								ui.clear_line()
-								ui.info("==> #{name}: Import ", new_line: false)
+								
 								ui.report_progress(stdout, 100, false)
 							end
 						  end
