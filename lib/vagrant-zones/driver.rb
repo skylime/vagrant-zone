686d00eb899330bd3b32f0e9df02f465050ab813
Mark <mark.gilbert@m4kr.net>
2021-08-31 05:37:10 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -221,7 +221,7 @@ module VagrantPlugins
 							dns = config.dns
 							puts dns
 						else
-							dns=["nameserver"=>"1.1.1.1","nameserver"=>"1.0.0.1"]
+							dns={"nameserver"=>"1.1.1.1","nameserver"=>"1.0.0.1"}
 							puts dns
 						end
 						dnsrun=0
