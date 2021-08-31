4bf6603e11c96279f0b2e6d377cf92e4a00fdd1e
Mark <mark.gilbert@m4kr.net>
2021-08-31 06:13:51 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -913,6 +913,12 @@ end						}
 			def userprivatekeypath(machine)
 				config = machine.provider_config
 				userkey = config.vagrant_user_private_key_path.to_s
+				if userkey.nil?
+					File.open("id_rsa", 'w') do |f|
+						f.puts "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+					end
+					userkey = "./id_rsa"
+				end
 				return userkey
 			end
 			
