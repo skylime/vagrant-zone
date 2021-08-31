53bb5cff80bc38bcb559fec35fecc08642145123
Mark <mark.gilbert@m4kr.net>
2021-08-31 06:59:11 -0500

    add retryable

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -12,6 +12,7 @@ require 'expect'
 require "vagrant"
 require 'vagrant-zones/util/timer'
 require 'vagrant-zones/util/subprocess'
+require 'vagrant/util/retryable'
 
 module VagrantPlugins
 	module ProviderZone
@@ -170,7 +171,6 @@ module VagrantPlugins
 						elsif opts[:dhcp] == false || opts[:dhcp].nil?
 							if opts[:managed]
 								ip = opts[:ip].to_s
-								puts ip
 								return nil if ip.length == 0
 								return ip.gsub /\t/, ''
 							end
@@ -861,7 +861,7 @@ end						}
 						zlogincommand(machine, "mkdir -p /home/vagrant/.ssh")
 						zlogincommand(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
 						id_rsa = "https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant"
-						command = "#{@pfexec} curl #{id_rsa}  -O id_rsa} "
+						command = "#{@pfexec} curl #{id_rsa}  -O id_rsa"
 						Util::Subprocess.new command do |stdout, stderr, thread|
 							ui.rewriting do |ui|
 								ui.clear_line()
