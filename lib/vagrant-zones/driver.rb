a55d9845a9caa00616800933dd161a39cfd20465
Mark <mark.gilbert@m4kr.net>
2021-08-31 05:41:46 -0500

    r

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -215,7 +215,7 @@ module VagrantPlugins
 						if !opts[:nictype].nil?
 							nictype  = opts[:nictype]
 						end
-						dns={nameserve: "1.1.1.1",nameserver: "1.0.0.1"}
+						dns={nameserver: "1.1.1.1",nameserver: "1.0.0.1"}
 						puts dns
 						if  !config.dns.nil? 
 							dns = config.dns
