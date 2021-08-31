58b9b2890ac25602ae7bbc082f22e28b7b4bb7bb
Mark <mark.gilbert@m4kr.net>
2021-08-31 12:58:58 -0500

    s

--- lib/vagrant-zones/action/start.rb
+++ lib/vagrant-zones/action/start.rb
@@ -4,7 +4,6 @@ require "log4r"
 module VagrantPlugins
 	module ProviderZone
 		module Action
-			# This is used to start the zone
 			class Start
 				def initialize(app, env)
 					@logger = Log4r::Logger.new("vagrant_zones::action::import")
@@ -16,7 +15,6 @@ module VagrantPlugins
 					@driver  = @machine.provider.driver
 					@driver.check_zone_support(@machine, env[:ui])
 					@driver.boot(@machine, env[:ui])
-					
 					@app.call(env)
 				end
 			end
