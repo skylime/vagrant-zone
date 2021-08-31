7bf10b0ff2b42e072cae3bebbb68ecaf4721660c
Mark <mark.gilbert@m4kr.net>
2021-08-31 08:08:23 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -358,7 +358,7 @@ end									}
 												if nic_number == devid
 													vnic=vmnic[devid.to_i]
 													## Get Device Mac Address for when Mac is not specified
-													if mac == 'auto'
+													if mac == "auto"
 														zlogin_write.printf("\nip link show dev #{vnic} | grep ether | awk '{ print $2 }'\n")
 														if responses[-1].to_s.match(/^(?:[[:xdigit:]]{2}([-:]))(?:[[:xdigit:]]{2}\1){4}[[:xdigit:]]{2}$/)	
 															mac = responses[-1][0][/^(?:[[:xdigit:]]{2}([-:]))(?:[[:xdigit:]]{2}\1){4}[[:xdigit:]]{2}$/]
