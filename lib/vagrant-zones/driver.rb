3258919ee67c5c273567f2b0fdfeb834e48a2984
Mark <mark.gilbert@m4kr.net>
2021-08-31 05:56:53 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -826,10 +826,6 @@ end						}
 				
 
 				if config.brand == 'lx'
-					zlogincommand(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant'))
-					zlogincommand(machine, "mkdir -p /home/vagrant/.ssh")
-					zlogincommand(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
-					zlogincommand(machine, "chown -R vagrant:vagrant /home/vagrant/.ssh")
 					zlogincommand(machine, "chmod 600 /home/vagrant/.ssh/authorized_keys")
 				elsif config.brand == 'bhyve'
 					network(@machine, ui, "setup")
@@ -865,6 +861,11 @@ end						}
 					zlogincommand(machine, %('echo nameserver 1.0.0.1 >> /etc/resolv.conf'))
 					if not user_exists?(machine)
 						zlogincommand(machine, "useradd -m -s /bin/bash -U vagrant")
+						zlogincommand(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant'))
+						zlogincommand(machine, "mkdir -p /home/vagrant/.ssh")
+						zlogincommand(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
+						zlogincommand(machine, "chown -R vagrant:vagrant /home/vagrant/.ssh")
+						zlogincommand(machine, "chmod 600 /home/vagrant/.ssh/authorized_keys")
 					end
 				end
 			end	
