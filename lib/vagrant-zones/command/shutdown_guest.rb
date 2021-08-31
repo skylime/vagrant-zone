2fd60e621b6f462d36f3e7b68d2582f9dd916bf5
Mark <mark.gilbert@m4kr.net>
2021-08-31 10:52:50 -0500

    s

--- lib/vagrant-zones/command/shutdown_guest.rb
+++ lib/vagrant-zones/command/shutdown_guest.rb
@@ -17,8 +17,7 @@ module VagrantPlugins
 
             ## Wait for VM up
             with_target_vms(argv, provider: :zone ) do |machine|
-                driver  = machine.provider.driver
-                driver.control(machine, @env.ui, 'shutdown')
+                machine.action('shutdown')
               end
           end
         end
