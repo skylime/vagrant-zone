16a3b462127c6cc60fc8c27cf024bc3b72fdbd37
Mark <mark.gilbert@m4kr.net>
2021-08-31 05:14:46 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -827,6 +827,14 @@ end						}
 				
 				state = "setup"
 				vnic(@machine, ui, state)
+
+				if config.brand == 'lx'
+					zlogincommand(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant'))
+					zlogincommand(machine, "mkdir -p /home/vagrant/.ssh")
+					zlogincommand(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
+					zlogincommand(machine, "chown -R vagrant:vagrant /home/vagrant/.ssh")
+					zlogincommand(machine, "chmod 600 /home/vagrant/.ssh/authorized_keys")
+				end
 				
 			end
 			
@@ -859,11 +867,6 @@ end						}
 					if not user_exists?(machine)
 						zlogincommand(machine, "useradd -m -s /bin/bash -U vagrant")
 					end
-					zlogincommand(machine, %('echo "vagrant ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant'))
-					zlogincommand(machine, "mkdir -p /home/vagrant/.ssh")
-					zlogincommand(machine, %('echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > /home/vagrant/.ssh/authorized_keys'))
-					zlogincommand(machine, "chown -R vagrant:vagrant /home/vagrant/.ssh")
-					zlogincommand(machine, "chmod 600 /home/vagrant/.ssh/authorized_keys")
 				end
 			end	
 				
