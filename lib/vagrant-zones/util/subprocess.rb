9e24eef80611e766cb4e641ff8def816df78505e
Mark <mark.gilbert@m4kr.net>
2021-09-05 06:43:45 -0500

    test

--- lib/vagrant-zones/util/subprocess.rb
+++ lib/vagrant-zones/util/subprocess.rb
@@ -7,7 +7,7 @@ module VagrantPlugins
     module Util
       # This is a utlity to start a subprocess
       class Subprocess
-        def initialize(cmd, &block)
+        def initialize(cmd, &_block)
           Open3.popen3(cmd) do |_stdin, stdout, stderr, thread|
             # read each stream from a new thread
             { :out => stdout, :err => stderr }.each do |key, stream|
