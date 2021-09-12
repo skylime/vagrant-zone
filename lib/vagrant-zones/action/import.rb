99699b545b13fcd5ae1b0db905c9390c594ff6be
Mark <mark.gilbert@m4kr.net>
2021-09-12 16:03:17 -0500

    ui.info(env[:machine].inspect)

--- lib/vagrant-zones/action/import.rb
+++ lib/vagrant-zones/action/import.rb
@@ -77,7 +77,7 @@ module VagrantPlugins
 					
 					else
 						# Support zss format only for now, use other images and convert later
-						ui.info(env[:machine].box.inspect)
+						ui.info(env[:machine].inspect)
 						box_format = env[:machine].box.metadata['format']
 						if box_format.nil?
 							raise Errors::NoBoxFormatSet
