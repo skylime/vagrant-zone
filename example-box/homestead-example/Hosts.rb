78579a824b69ccb81e544a36ecc45b6e575cc43a
Mark <mark.gilbert@m4kr.net>
2021-09-12 17:27:49 -0500

    server.vm.box

--- example-box/homestead-example/Hosts.rb
+++ example-box/homestead-example/Hosts.rb
@@ -12,7 +12,7 @@ class Hosts
         autostart = host.has_key?('autostart') && host['autostart']
         config.vm.define "#{host['parition_id']}-#{host['name']}", autostart: autostart do |server|
           server.vm.box = host['box']
-
+          puts server.vm.box 
           
           server.vm.boot_timeout = 900
   
