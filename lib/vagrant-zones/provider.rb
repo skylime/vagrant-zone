f8292df54e951c1b0f07ea52d34ad0985ca517bb
Mark <mark.gilbert@m4kr.net>
2021-09-13 05:47:50 -0500

    provider udpate test

--- lib/vagrant-zones/provider.rb
+++ lib/vagrant-zones/provider.rb
@@ -24,25 +24,14 @@ module VagrantPlugins
         # We just return nil if were not able to identify the VM's IP and
         # let Vagrant core deal with it like docker provider does
         return nil if state.id != :running
-
-        ip = driver.get_ip_address(@machine)
-        user = driver.user(@machine)
-        userkey = driver.userprivatekeypath(@machine).to_s
-        vagrantuserpassword = driver.vagrantuserpass(@machine).to_s
-        passwordauth = 'yes'
-        return nil unless ip
-
-        portnumber = '22'
-        portnumber = driver.sshport(@machine).to_s unless portnumber.to_s.nil? || portnumber.to_i.zero?
         ssh_info = {
-          host: ip,
-          port: portnumber,
-          password: vagrantuserpassword,
-          username: user,
-          private_key_path: userkey,
+          host: "#{driver.get_ip_address(@machine)}",
+          port: "#{driver.sshport(@machine)}",
+          password: "#{driver.vagrantuserpass(@machine)}",
+          username: "#{driver.user(@machine)}",
+          private_key_path: "#{driver.userprivatekeypath(@machine)}",
           PasswordAuthentication: 'passwordauth'
         }
-        puts "horray" unless ssh_info.nil?
       end
 
       # This should return an action callable for the given name.
