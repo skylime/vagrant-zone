a16a3beb97fbce2b9471b98e01867a420005ab43
Mark <mark.gilbert@m4kr.net>
2021-09-13 02:05:35 -0500

    lint

--- lib/vagrant-zones/errors.rb
+++ lib/vagrant-zones/errors.rb
@@ -8,30 +8,39 @@ module VagrantPlugins
       class VagrantZonesError < Vagrant::Errors::VagrantError
         error_namespace('vagrant_zones.errors')
       end
+      
       class SystemVersionIsTooLow < VagrantZonesError
         error_key(:system_version_too_low)
       end
+
       class MissingCompatCheckTool < VagrantZonesError
         error_key(:missing_compatability_check_tool)
-      end      
+      end
+
       class MissingBhyve < VagrantZonesError
         error_key(:missing_bhyve)
       end
+      
       class HasNoRootPrivilege < VagrantZonesError
         error_key(:has_no_root_privilege)
       end
+
       class ExecuteError < VagrantZonesError
         error_key(:execute_error)
       end
+
       class TimeoutError < VagrantZonesError
         error_key(:timeout_error)
       end
+
       class VirtualBoxRunningConflictDetected < VagrantZonesError
         error_key(:virtual_box_running_conflict_detected)
       end
+
       class NotYetImplemented < VagrantZonesError
         error_key(:not_yet_implemented)
       end
+
       class InvalidBrand < VagrantZonesError
         error_key(:invalid_brand)
       end
