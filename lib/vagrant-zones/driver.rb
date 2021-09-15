e8d5c32f3acdf277802caa52e75f9d4a75dede22
Mark <mark.gilbert@m4kr.net>
2021-09-15 03:44:14 -0500

    testt

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -444,6 +444,8 @@ end             )
         ## Create Additional Disks
         unless config.additional_disks.nil?
           inspect config.additional_disks
+          puts config.additional_disks.length
+          puts config.additional_disks
           config.additional_disks.each do |disk|
             cinfo = "#{disk['size']}, #{disk['array']}#{disk['path']}"
             uiinfo.info(I18n.t('vagrant_zones.bhyve_zone_dataset_additional_volume') + cinfo)
@@ -463,6 +465,9 @@ end             )
         if dataset_boot_exists == "#{zp}/boot"
           ## Destroy Additional Disks
           unless  config.additional_disks.nil?
+            inspect config.additional_disks
+            puts config.additional_disks.length
+            puts config.additional_disks
             disks = config.additional_disks
             disks.each do |disk|
               addataset = "#{disk['array']}#{disk['path']}"
