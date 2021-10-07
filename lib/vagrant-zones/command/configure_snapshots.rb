b1896a8ca0c9b6845bd88f73c932e53746efd17f
Mark <mark.gilbert@m4kr.net>
2021-10-07 08:21:48 -0500

    save

--- lib/vagrant-zones/command/configure_snapshots.rb
+++ lib/vagrant-zones/command/configure_snapshots.rb
@@ -47,6 +47,7 @@ module VagrantPlugins
 
           with_target_vms(argv, provider: :zone) do |machine|
             driver = machine.provider.driver
+            subcommanddata = []
             subcommanddata =  options[:frequency] unless options[:delete] || options[:list]
             subcommanddata << options[:frequency_retention] unless options[:delete] || options[:list]
             subcommanddata =  options[:list] unless options[:delete] || options[:frequency]
