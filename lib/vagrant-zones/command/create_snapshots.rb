4d1a4c8f7a0e051e6987e4e95b97bad4fd1cf3f0
Mark <mark.gilbert@m4kr.net>
2021-10-07 01:30:20 -0500

    save

--- lib/vagrant-zones/command/create_snapshots.rb
+++ lib/vagrant-zones/command/create_snapshots.rb
@@ -20,6 +20,7 @@ module VagrantPlugins
           argv = parse_options(opts)
           return unless argv
 
+          puts argv
           unless argv.length <= 4
             @env.ui.info(opts.help)
             return
