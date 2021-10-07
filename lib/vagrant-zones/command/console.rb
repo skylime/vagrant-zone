e6442be516410d22e1897c0272fc0a2dac12f90b
Mark <mark.gilbert@m4kr.net>
2021-10-07 01:01:20 -0500

    save

--- lib/vagrant-zones/command/console.rb
+++ lib/vagrant-zones/command/console.rb
@@ -43,8 +43,8 @@ module VagrantPlugins
             else
               command_class = @subcommands.get(machine.provider_config.console.to_sym) if @sub_command
               subargs = ""
-              subargs = @sub_args.inspect unless @sub_args.inspect.nil?
-              puts "Invoking command class: #{command_class} #{subargs} "
+              subargs = @sub_args unless @sub_args.inspect.nil?
+              puts "Invoking command class: #{command_class} #{subargs.inspect} "
               
               @logger.debug("Invoking command class: #{command_class} #{machine.provider_config.console.to_sym}")
               # Initialize and execute the command class
