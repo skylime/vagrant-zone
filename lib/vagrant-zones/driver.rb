4330a09d744f598835655e0d3d11fc65838636c6
Mark <mark.gilbert@m4kr.net>
2021-10-07 06:59:29 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1002,7 +1002,7 @@ end          )
               if snapindex == 0
                 puts sprintf '%8s  %-*s  %-*s  %-*s  %-*s  %-*s', "#",  snapmaxlength, attributes[0], usedmaxlength, attributes[1], availmaxlength, attributes[2], refermaxlength, attributes[3],pathmaxlength, attributes[4]
               else
-                puts sprintf '%8s  %-*s  %-*s  %-*s  %-*s  %-*s', snapindex - 2, snapmaxlength, attributes[0], usedmaxlength, attributes[1], availmaxlength, attributes[2], refermaxlength, attributes[3],pathmaxlength, attributes[4]
+                puts sprintf '%8s  %-*s  %-*s  %-*s  %-*s  %-*s', snapindex - 1, snapmaxlength, attributes[0], usedmaxlength, attributes[1], availmaxlength, attributes[2], refermaxlength, attributes[3],pathmaxlength, attributes[4]
               end
               output = {}
             end
