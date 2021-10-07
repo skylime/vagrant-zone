074384c0953a46f675c51585395ee66126fc79fe
Mark <mark.gilbert@m4kr.net>
2021-10-07 00:51:56 -0500

    s

--- lib/vagrant-zones/command/console.rb
+++ lib/vagrant-zones/command/console.rb
@@ -35,7 +35,7 @@ module VagrantPlugins
               command_class = @subcommands.get(@sub_command.to_sym) if @sub_command
               return help if !command_class || !@sub_command
   
-              puts "Invoking command class: #{command_class} #{@sub_args.inspect}")
+              puts "Invoking command class: #{command_class} #{@sub_args.inspect}"
   
               # Initialize and execute the command class
               command_class.new(@sub_args, @env).execute
