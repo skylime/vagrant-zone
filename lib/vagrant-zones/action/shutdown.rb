47b69d9824b56667eafc351172f70d1f0c8ad1a9
Mark <mark.gilbert@m4kr.net>
2021-10-06 23:27:35 -0500

    halt if hung

--- lib/vagrant-zones/action/shutdown.rb
+++ lib/vagrant-zones/action/shutdown.rb
@@ -18,6 +18,7 @@ module VagrantPlugins
         def call(env)
           @machine = env[:machine]
           @driver  = @machine.provider.driver
+          name = machine.name
           ui = env[:ui]
 
           ui.info(I18n.t('vagrant_zones.graceful_shutdown_started'))
@@ -36,7 +37,15 @@ module VagrantPlugins
             end
           end
           ui.info(I18n.t('vagrant_zones.graceful_shutdown_complete'))
-
+          env[:metrics] ||= {}
+          env[:metrics]['instance_ssh_time'] = Util::Timer.time do
+            retryable(on: Errors::TimeoutError, tries: 300) do
+              # If we're interrupted don't worry about waiting
+              next if env[:interrupted]
+              vm_state = 'installed'
+              break if execute(false, "pfexec zoneadm -z #{name} list -p | awk -F: '{ print $3 }'") == vm_state
+            end
+          end
           @driver.halt(@machine, env[:ui])
           @app.call(env)
         end
