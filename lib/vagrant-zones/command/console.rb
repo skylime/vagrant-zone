280a02b8e073c3482ed456dc8659fae121e9c776
Mark <mark.gilbert@m4kr.net>
2021-10-07 01:47:44 -0500

    s

--- lib/vagrant-zones/command/console.rb
+++ lib/vagrant-zones/command/console.rb
@@ -42,10 +42,10 @@ module VagrantPlugins
             else
               @sub_command = machine.provider_config.console.to_sym  if @sub_command.nil?
               command_class = @subcommands.get(@sub_command.to_sym) if @sub_command
-              subargs = ['--kill no'] if @sub_args.nil?
+              subargs = ['--kill no'] 
+              puts subargs
               subargs = @sub_args if @sub_args
               
-              puts subargs.inspect
               @logger.debug("Invoking command class: #{command_class} #{machine.provider_config.console.to_sym}")
               # Initialize and execute the command class
               command_class.new(subargs, @env).execute
