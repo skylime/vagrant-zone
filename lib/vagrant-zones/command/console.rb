87df4d8f4f376825fa9d3d210c235f4b218a4578
Mark <mark.gilbert@m4kr.net>
2021-10-07 00:51:52 -0500

    save

--- lib/vagrant-zones/command/console.rb
+++ lib/vagrant-zones/command/console.rb
@@ -32,25 +32,18 @@ module VagrantPlugins
 
           with_target_vms(@main_args,provider: :zone) do |machine|
             if machine.provider_config.console.nil?
-              puts @sub_command
-              puts @sub_command.to_sym
-              puts machine.provider_config.console
-              puts machine.provider_config.console.to_sym
               command_class = @subcommands.get(@sub_command.to_sym) if @sub_command
               return help if !command_class || !@sub_command
   
-              @logger.debug("Invoking command class: #{command_class} #{@sub_args.inspect}")
+              puts "Invoking command class: #{command_class} #{@sub_args.inspect}")
   
               # Initialize and execute the command class
               command_class.new(@sub_args, @env).execute
 
             else
-              puts @sub_command
-              puts @sub_command
-              puts machine.provider_config.console
-              puts machine.provider_config.console.to_sym
-              command_class = @subcommands.get(@sub_command.to_sym) if @sub_command
-              @logger.debug("Invoking command class: #{command_class} #{@sub_args.inspect}")
+
+              command_class = @subcommands.get(machine.provider_config.console.to_sym) if @sub_command
+              @logger.debug("Invoking command class: #{command_class} #{machine.provider_config.console.to_sym}")
               # Initialize and execute the command class
               command_class.new(@sub_args, @env).execute
 
