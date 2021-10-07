a874a082582f5671d149cc1f766ea7e59e1d38f7
Mark <mark.gilbert@m4kr.net>
2021-10-07 06:55:51 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1044,7 +1044,7 @@ end          )
                 output = execute(false, "#{@pfexec} zfs list -t snapshot -o name | grep #{disk}")
                 output = output.split(/\n/).drop(1)
                 output.each_with_index do |snaps, spindex|
-                  if snapshot_name.to_i == spindex && !snapshot_name.to_s == 'all'
+                  if snapshot_name.to_i == spindex && snapshot_name.to_s != 'all'
                     puts snaps, spindex
                     execute(false, "#{@pfexec} zfs destroy #{snaps}") 
                     uiinfo.info(I18n.t('vagrant_zones.zfs_snapshot_destroy'))
