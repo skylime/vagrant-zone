763332ee76872ad23da29c00586d68b0997820c1
Mark <mark.gilbert@m4kr.net>
2021-10-16 07:24:53 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -995,7 +995,7 @@ end          )
             zfssnapshots.reverse.each_with_index do |snapshot, snapindex|
               attributes = snapshot.gsub(/\s+/m, ' ').strip.split
               if snapindex.zero?
-                puts format('%<snaplength>8s %<usedmax>-*s %<availmax>s %<refermax>s ', snaplength: '#' ,  usedmax: attributes[0], snapmaxlength: snapmaxlength,  availmax: attributes[1],refermax: attributes[0])
+                puts format('%<snaplength>8s %<usedmax>-<snapmaxlength>s %<availmax>s %<refermax>s ', snaplength: '#' ,  usedmax: attributes[0], snapmaxlength: snapmaxlength,  availmax: attributes[1],refermax: attributes[0])
                # puts format( '%<snapmaxlength>8s  %-*s  %-*s  %-*s  %-*s  %-*s', '#',  snapmaxlength, attributes[0], usedmaxlength, attributes[1], availmaxlength, attributes[2], refermaxlength, attributes[3], pathmaxlength, attributes[4] )
               else
                 puts sprintf '%8s  %-*s  %-*s  %-*s  %-*s  %-*s', snapindex - 2, snapmaxlength, attributes[0], usedmaxlength, attributes[1], availmaxlength, attributes[2], refermaxlength, attributes[3], pathmaxlength, attributes[4]
