a28ee3f6223c1b913209f18a7997b6194b3d9785
Mark <mark.gilbert@m4kr.net>
2021-10-16 09:42:15 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -976,7 +976,8 @@ end          )
           datasets.each_with_index do |disk, index|
             puts "\n  Disk Number: #{index}\n  Disk Path: #{disk}"
             zfs_snapshots = execute(false, "#{@pfexec} zfs list -t snapshot | grep #{disk}")
-            return if zfs_snapshots.nil?
+            next if zfs_snapshots.nil?
+
             zfssnapshots = zfs_snapshots.split(/\n/)
             zfssnapshots = zfssnapshots.reverse
             zfssnapshots << "Snapshot\t\t\t\tUsed\tAvailable\tRefer\tPath"
