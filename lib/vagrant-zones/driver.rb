a2099251851407dfb5a1f979a0ea4b7f7553c536
Mark <mark.gilbert@m4kr.net>
2021-10-17 13:39:10 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1088,7 +1088,7 @@ end          )
              monthlycron = "0  0  1  *  *   #{snapshooter} -p monthly -r -n #{monthlytrn} #{disk}  # #{machine.name}"
 
              crons.each do |job|
-              next if job == ""
+              next if job.empty?
               
               puts job.inspect
               puts
