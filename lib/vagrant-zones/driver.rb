a9dea6412e4869cbd10a73a962351cecedb3802b
Mark <mark.gilbert@m4kr.net>
2021-10-19 16:49:39 -0500

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1141,7 +1141,7 @@ end          )
               end
             elsif options[:set_frequency]
               if options[:set_frequency] == 'all'
-                if options[:set_frequency_rtn] && options[:set_frequency_rtn] != 'defaults'
+                unless options[:set_frequency_rtn].nil? && options[:set_frequency_rtn] == 'defaults'
 
                   puts options[:set_frequency_rtn]
                   puts hourlycron unless cronjobs[:hourly] 
