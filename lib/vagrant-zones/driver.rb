4177ebce6554df55bb0313ecdb38d71122eefd20
Mark <mark.gilbert@m4kr.net>
2021-10-19 17:16:37 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1143,35 +1143,29 @@ end          )
               if options[:set_frequency] == 'all'
                 if options[:set_frequency_rtn].nil? || options[:set_frequency_rtn] == 'defaults'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{hourlycron}'; } | #{@pfexec} crontab" unless cronjobs[:hourly] && options[:set_frequency] == 'hourly'
-                  puts setcron
+                  puts setcron unless cronjobs[:hourly] && options[:set_frequency] == 'hourly'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{dailycron}'; } | #{@pfexec} crontab" unless cronjobs[:daily] && options[:set_frequency] == 'daily'
-                  puts setcron
+                  puts setcron unless cronjobs[:daily] && options[:set_frequency] == 'daily'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{weeklycron}'; } | #{@pfexec} crontab" unless cronjobs[:weekly] && options[:set_frequency] == 'weekly'
-                  puts setcron
+                  puts setcron unless cronjobs[:weekly] && options[:set_frequency] == 'weekly'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{monthlycron}'; } | #{@pfexec} crontab" unless cronjobs[:monthly] && options[:set_frequency] == 'monthly'
-                  puts setcron
-                  puts setcron
+                  puts setcron unless cronjobs[:monthly] && options[:set_frequency] == 'monthly'
                 else
                   hourlycron = "0  1-23  *  *  *  #{snapshooter} -p hourly -r -n #{options[:set_frequency_rtn]} #{disk}  # #{machine.name}"
                   dailycron = "0  0  *  *  0-5  #{snapshooter} -p daily -r -n #{options[:set_frequency_rtn]} #{disk}  # #{machine.name}"
                   weeklycron = "0  0  *  *  6   #{snapshooter} -p weekly -r -n #{options[:set_frequency_rtn]} #{disk}  # #{machine.name}"
                   monthlycron = "0  0  1  *  *   #{snapshooter} -p monthly -r -n #{options[:set_frequency_rtn]} #{disk}  # #{machine.name}"
                   setcron = "{ #{@pfexec} crontab -l; echo '#{hourlycron}'; } | #{@pfexec} crontab" unless cronjobs[:hourly] && options[:set_frequency] == 'hourly'
-                  puts setcron
+                  puts setcron unless cronjobs[:hourly] && options[:set_frequency] == 'hourly'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{dailycron}'; } | #{@pfexec} crontab" unless cronjobs[:daily] && options[:set_frequency] == 'daily'
-                  puts setcron
+                  puts setcron unless cronjobs[:daily] && options[:set_frequency] == 'daily'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{weeklycron}'; } | #{@pfexec} crontab" unless cronjobs[:weekly] && options[:set_frequency] == 'weekly'
-                  puts setcron
+                  puts setcron unless cronjobs[:weekly] && options[:set_frequency] == 'weekly'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{monthlycron}'; } | #{@pfexec} crontab" unless cronjobs[:monthly] && options[:set_frequency] == 'monthly'
-                  puts setcron
+                  puts setcron unless cronjobs[:monthly] && options[:set_frequency] == 'monthly'
                 end
               else
                 if options[:set_frequency_rtn].nil? || options[:set_frequency_rtn] == 'defaults'
-
-                  puts hourlycron unless cronjobs[:hourly]
-                  puts dailycron unless cronjobs[:daily]
-                  puts weeklycron unless cronjobs[:weekly]
-                  puts monthlycron unless cronjobs[:monthly]
                   setcron = "{ #{@pfexec} crontab -l; echo '#{hourlycron}'; } | #{@pfexec} crontab" if cronjobs[:hourly].nil? && options[:set_frequency] == 'hourly'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{dailycron}'; } | #{@pfexec} crontab" if cronjobs[:daily].nil? && options[:set_frequency] == 'daily'
                   setcron = "{ #{@pfexec} crontab -l; echo '#{weeklycron}'; } | #{@pfexec} crontab" if cronjobs[:weekly].nil? && options[:set_frequency] == 'weekly'
