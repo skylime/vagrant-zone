eb3f2ab01d70ac1c6b19f9a6e0dc4099446ec6b6
Mark <mark.gilbert@m4kr.net>
2021-10-19 17:50:37 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1129,10 +1129,10 @@ end          )
                 removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:monthly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" unless cronjobs[:monthly].nil?
                 puts removecron  unless cronjobs[:monthly].nil?
               else
-                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:hourly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" unless cronjobs[:hourly].nil? && options[:delete] == 'hourly' 
-                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:daily].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" unless cronjobs[:daily].nil? && options[:delete] == 'daily'
-                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:weekly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }"   unless cronjobs[:weekly].nil? && options[:delete] == 'weekly'
-                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:monthly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" unless cronjobs[:monthly].nil? && options[:delete] == 'monthly' 
+                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:hourly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" if cronjobs[:hourly].nil? && options[:delete] == 'hourly' 
+                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:daily].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" if cronjobs[:daily].nil? && options[:delete] == 'daily'
+                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:weekly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }"   if cronjobs[:weekly].nil? && options[:delete] == 'weekly'
+                removecron = "{ #{@pfexec} crontab -l | grep -v '#{cronjobs[:monthly].gsub(/\*/, '\*')}'  | #{@pfexec} crontab }" if cronjobs[:monthly].nil? && options[:delete] == 'monthly' 
                 puts removecron
               end
             elsif options[:set_frequency]
