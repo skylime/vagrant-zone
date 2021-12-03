c4bfa61ca4ef629034a8d35f38924b5f280e3f63
Mark <mark.gilbert@m4kr.net>
2021-12-03 04:01:01 -0600

    wrong placement for sparse a nd blocksize

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -363,7 +363,7 @@ end             )
           # Create boot volume
           cinfo = "#{bootconfigs['size']}, #{datasetroot}"
           uiinfo.info(I18n.t('vagrant_zones.bhyve_zone_dataset_boot') + cinfo)
-          execute(false, "#{@pfexec} zfs create -V -s -b 4k #{bootconfigs['size']} #{datasetroot}")
+          execute(false, "#{@pfexec} zfs create -b 4k -s -V   #{bootconfigs['size']} #{datasetroot}")
 
           ## Import template to boot volume
           uiinfo.info(I18n.t('vagrant_zones.bhyve_zone_dataset_boot_volume') + datasetroot)
@@ -389,7 +389,7 @@ end             )
           dataset = "#{disk['array']}/#{disk['dataset']}/#{config.name}/#{disk['volume_name']}"
           cinfo = ",#{disk['size']}, #{dataset}"
           uiinfo.info(I18n.t('vagrant_zones.bhyve_zone_dataset_additional_volume') + cinfo)
-          execute(false, "#{@pfexec} zfs create -V -s #{disk['size']} #{dataset}")
+          execute(false, "#{@pfexec} zfs create -b 4k -s -V #{disk['size']} #{dataset}")
         end
       end
 
