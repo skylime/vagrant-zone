74477021e13b84c692a76d76a0e3666b98c62a53
Mark <mark.gilbert@m4kr.net>
2021-12-26 12:23:40 -0600

    update delete scripts

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -433,7 +433,7 @@ end             )
               cinfo = ", #{disk['array']}/#{disk['dataset']}/#{name}"
               uiinfo.info(I18n.t('vagrant_zones.bhyve_zone_dataset_additional_volume') + cinfo)
               addtl_dataset_root_exists = execute(false, "#{@pfexec} zfs list | grep #{disk['array']}/#{disk['dataset']}/#{name} | awk '{ print $1 }' || true")
-              execute(false, "#{@pfexec} zfs destroy #{disk['array']}/#{disk['dataset']}/#{name}")   if addtl_dataset_root_exists == "#{disk['array']}/#{disk['dataset']}/#{name}"
+              execute(false, "#{@pfexec} zfs destroy #{disk['array']}/#{disk['dataset']}/#{name}")   if addtl_dataset_root_exists != zp
             end
 
           end
