64f5c9db2bba7627697544ef08f64039858e841c
Mark <mark.gilbert@m4kr.net>
2021-12-26 23:52:49 -0600

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1032,9 +1032,9 @@ module VagrantPlugins
               cronjobs.merge!(monthly: monthly)
             end
           end
-          zfssnapcronlist(datasets, _config, opts, uiinfo, cronjobs)
-          zfssnapcrondelete(datasets, _config, opts, uiinfo, cronjobs)
-          zfssnapcronset(datasets, _config, opts, uiinfo, cronjobs)
+          zfssnapcronlist(datasets, config, opts, uiinfo, cronjobs)
+          zfssnapcrondelete(datasets, config, opts, uiinfo, cronjobs)
+          zfssnapcronset(datasets, config, opts, uiinfo, cronjobs)
         end
       end
       ####################### REFACTOR THIS ###############################
