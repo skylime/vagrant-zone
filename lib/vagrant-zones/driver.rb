a3c598404a3bfbbcc7650e4d22ffde6f5e092040
Mark <mark.gilbert@m4kr.net>
2021-12-28 08:40:16 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -259,7 +259,7 @@ module VagrantPlugins
           config.dns.each do |ns|
             dservers.append(ns['nameserver'])
           end
-          puts dservers[0]
+          puts "Servers: #{dservers.join(',')}\n\n"
 
           dns = [{ 'nameserver' => '1.1.1.1' }, { 'nameserver' => '8.8.8.8' }] if config.dns.nil?
           servers = []
