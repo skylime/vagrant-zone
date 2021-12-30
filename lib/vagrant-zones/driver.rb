68c60cdd3ed67c03e761e9195881bf6ccfd9c000
Mark <mark.gilbert@m4kr.net>
2021-12-29 20:16:38 -0600

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -752,7 +752,6 @@ module VagrantPlugins
           rsp = []
           PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read, zlogin_write, pid|
           zlogin_write.printf("\n")
-          ###################### NEW ###################
             Timeout.timeout(config.setup_wait) do
               loop do
                 zlogin_read.expect(/\r\n/) { |line| rsp.push line }
@@ -765,6 +764,7 @@ module VagrantPlugins
                 alm = false if rsp[-1].to_s.match(/#{lcheck}/)
                 break if rsp[-1].to_s.match(/#{lcheck}/)
 
+                zlogin_write.printf("\n") if rsp[-1].to_s.match(/\r\n/)
               end
             end          
             Process.kill('HUP', pid) if loginstring.expect(/#{lcheck}/) || loginstring.expect(/#{almatch}/)
