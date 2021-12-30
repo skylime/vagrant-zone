3241d40af4387c8e18b9d253da168de167f13b0b
Mark <mark.gilbert@m4kr.net>
2021-12-30 08:45:25 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1176,14 +1176,14 @@ module VagrantPlugins
         h.each do |k, d|
           puts cronjobs[k]
           next if cronjobs[k].nil?
-          
+
           cj = "#{d[:ct]}#{snpshtr} -p #{k} -r -n #{sf[:rtn]} #{disk} # #{name}" unless sf[:rtn].nil?
           cj = "#{d[:ct]}#{snpshtr} -p #{k} -r -n #{d[:rtn]} #{disk} # #{name}" if sf[:rtn].nil?
           h[k] = { rtn: rtn[:h], ct: ct[:h], cj: cj }
           
           setcron = "#{shrtcr}'#{cj}' ) | #{@pfexec} crontab" if cronjobs[k].nil?
-          uii.info("Setting Cron: #{setcron}\n") if k.to_s == sf[:freq] || sf[:freq] == 'all'
-          execute(false, setcron)  if k.to_s == sf[:freq] || sf[:freq] == 'all'
+          uii.info("Setting Cron: #{setcron}\n") if (k.to_s == sf[:freq] || sf[:freq] == 'all') && cronjobs[k].nil?
+          execute(false, setcron) if (k.to_s == sf[:freq] || sf[:freq] == 'all') && cronjobs[k].nil?
         end
       end
 
