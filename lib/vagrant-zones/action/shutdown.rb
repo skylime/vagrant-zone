d3c8ef791bbe1fc4805238c3f1c085917d0a69b1
Mark <mark.gilbert@m4kr.net>
2021-12-30 02:43:53 -0600

    save

--- lib/vagrant-zones/action/shutdown.rb
+++ lib/vagrant-zones/action/shutdown.rb
@@ -32,25 +32,18 @@ module VagrantPlugins
               break unless env[:machine].communicate.ready?
             end
           end
-          
-          10.times do
-            state_id = @driver.state(@machine)
-            sleep 10 if state_id == 'running'
-            puts state_id
-          end
-          ui.info(I18n.t('vagrant_zones.graceful_shutdown_complete'))
+                    ui.info(I18n.t('vagrant_zones.graceful_shutdown_complete'))
           env[:metrics] ||= {}
           env[:metrics]['instance_ssh_time'] = Util::Timer.time do
             retryable(on: Errors::TimeoutError, tries: 300) do
               # If we're interrupted don't worry about waiting
               vm_state = @driver.state(@machine)
               sleep 10 if vm_state == 'running'
+              puts vm_state
               next if env[:interrupted]
               break unless vm_state == 'running'
             end
           end
-          state_id = @driver.state(@machine)
-          puts state_id
           @driver.halt(env[:ui])
           @app.call(env)
         end
