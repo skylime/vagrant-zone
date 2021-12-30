669058b9b32642beb4f040388cf5a65d86acfa84
Mark <mark.gilbert@m4kr.net>
2021-12-30 07:42:18 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1178,9 +1178,9 @@ module VagrantPlugins
           cj = "#{d[:ct]}#{snpshtr} -p #{k.to_s} -r -n #{sfr} #{disk} # #{name}" unless sfr.nil?
           cj = "#{d[:ct]}#{snpshtr} -p #{k.to_s} -r -n #{d[:rtn]} #{disk} # #{name}" if sfr.nil?
           h[k] = {rtn: rtn[:h], ct: ct[:h], cj: cj}
-          puts k if opts[:set_frequency] == 'all' || opts[:set_frequency]
-          puts d if opts[:set_frequency] == 'all' || opts[:set_frequency]
-          uii.info(d[:cj]) if opts[:set_frequency] == 'all' || opts[:set_frequency]     
+          puts k if k == 'all' || opts[:set_frequency]
+          puts d if k == 'all' || opts[:set_frequency]
+          uii.info(d[:cj]) if k == 'all' || opts[:set_frequency]     
           # setcron = "#{shrtcr}'#{hourlycron}' ) | #{@pfexec} crontab" if cronjobs[:hourly].nil?
           
           # execute(false, setcron)
