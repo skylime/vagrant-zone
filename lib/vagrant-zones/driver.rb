0052cc87d7dbcf23b2592172c24930a982104330
Mark <mark.gilbert@m4kr.net>
2021-12-30 06:02:42 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1169,7 +1169,7 @@ module VagrantPlugins
         
         
         
-        hourlycron = "0 1-23 * * * #{snpshtr} -p hourly -r -n #{rtn[:hourly]} #{disk} # #{name}"
+        hourlycron = "0 1-23 * * * #{snpshtr} -p hourly -r -n #{rtn[:hourly].to_s} #{disk} # #{name}"
         dailycron = "0 0 * * 0-5 #{snpshtr} -p daily -r -n #{rtn[:daily].to_s} #{disk} # #{name}"
         weeklycron = "0 0 * * 6 #{snpshtr} -p weekly -r -n #{rtn[:weekly].to_s} #{disk} # #{name}"
         monthlycron = "0 0 1 * * #{snpshtr} -p monthly -r -n #{rtn[:monthly].to_s} #{disk} # #{name}"
