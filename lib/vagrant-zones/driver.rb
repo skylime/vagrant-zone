eb07ec9c10159cb60768f6197058271c5f39f917
Mark <mark.gilbert@m4kr.net>
2021-12-31 01:30:46 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1019,8 +1019,8 @@ module VagrantPlugins
 
           if opts[:dataset] # either a number or a dataset
             #next if opts[:dataset].to_i != index
-            puts "is a int" if opts[:dataset].is_a? Numeric
-            puts "is a string or nil" unless opts[:dataset].is_a? Numeric
+            puts "is a int" if opts[:dataset].is_a? Integer
+            puts "is a string or nil" unless opts[:dataset].is_a? Integer
           end
 
           uii.info("\n Disk Number: #{index}\n Disk Path: #{disk}")
