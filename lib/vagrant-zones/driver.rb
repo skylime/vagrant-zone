7496d3a50fa144300cf1e1684b10a8c6b2063204
Mark <mark.gilbert@m4kr.net>
2021-12-30 22:21:04 -0600

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1144,7 +1144,7 @@ module VagrantPlugins
 
         h = { h: 'hourly', d: 'daily', w: 'weekly', m: 'monthly' }
         h.each do |k, d|
-          next if cronjobs[d.to_sym].nil? || opts[:delete] != d.to_s || opts[:delete] != 'all'
+          next if cronjobs[d.to_sym].nil?
 
           rc = "#{rmcr}'#{cronjobs[d.to_sym].to_s.gsub(/\*/, '\*')}' | #{sc}"
           uii.info("Removing Cron: #{rc}\n") 
