e9b48909cd01f346eef494e2bb3ae56b79315b7b
Mark <mark.gilbert@m4kr.net>
2021-12-31 00:01:23 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1146,10 +1146,6 @@ module VagrantPlugins
           next if cronjobs[d.to_sym].nil?
           
 
-
-          puts opts[:delete]
-          puts d
-
           next unless opts[:delete] == d || opts[:delete] == 'all'
 
           rc = "#{rmcr}'#{cronjobs[d.to_sym].to_s.gsub(/\*/, '\*')}' | #{sc}" if opts[:delete] == d || opts[:delete] == 'all'
