35560c24590d743735e14bd8bac4cf9425ceef21
Mark <mark.gilbert@m4kr.net>
2021-12-31 00:00:11 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1145,15 +1145,10 @@ module VagrantPlugins
           # next if (cronjobs[d.to_sym].nil? || opts[:delete] != d.to_s) 
           next if cronjobs[d.to_sym].nil?
           
-
-
-          puts opts[:delete]
-          puts d
-
           next unless opts[:delete] == d || opts[:delete] == 'all'
 
-          rc = "#{rmcr}'#{cronjobs[d.to_sym].to_s.gsub(/\*/, '\*')}' | #{sc}" if opts[:delete] == d.to_s || 'all'
-          uii.info("Removing Cron: #{rc}\n")  if opts[:delete].to_s == d.to_s || 'all'
+          rc = "#{rmcr}'#{cronjobs[d.to_sym].to_s.gsub(/\*/, '\*')}' | #{sc}" unless opts[:delete] == d || opts[:delete] == 'all'
+          uii.info("Removing Cron: #{rc}\n") unless opts[:delete] == d || opts[:delete] == 'all'
           # execute(false, rc)
         end
       end
