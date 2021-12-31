1495381692b7302a76cc585fc721fccd56bab0a4
Mark <mark.gilbert@m4kr.net>
2021-12-30 22:03:51 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1145,9 +1145,8 @@ module VagrantPlugins
         ###########################################
         h = { h: 'hourly', d: 'daily', w: 'weekly', m: 'monthly' }
         h.each do |k, d|
-          puts cronjobs[d.to_sym]
-          #rc = "#{rmcr}'#{cronjobs[d].gsub(/\*/, '\*')}' | #{sc}" 
-          #puts rc 
+          rc = "#{rmcr}'#{cronjobs[d.to_sym].gsub(/\*/, '\*')}' | #{sc}" 
+          puts rc 
           #rc = "#{shrtcr}'#{cj}' ) | #{@pfexec} crontab" if (k.to_s == sf[:freq] || sf[:freq] == 'all') && cronjobs[k].nil?
           #rc = "#{rmcr}'#{cronjobs[:hourly].gsub(/\*/, '\*')}' | #{sc}" if cronjobs[:hourly] && opts[:delete] == 'hourly'
           #uii.info("Setting Cron: #{setcron}\n") if (k.to_s == sf[:freq] || sf[:freq] == 'all') && cronjobs[k].nil?
