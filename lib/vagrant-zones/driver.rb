8cf7ac1ec0ec38b4a18354dd8d7fcb99d1f1c9bb
Mark <mark.gilbert@m4kr.net>
2021-12-31 09:33:27 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -298,9 +298,9 @@ module VagrantPlugins
             zoneniccreate(uii, opts) if state == 'create'
             zonecfgnicconfig(uii, opts) if state == 'config'
             ## if state setup and setup_method dhcp/zlogin
-            puts config.setup_method
-            puts config.setup_method
-            puts config.setup_method
+            puts config.setup_method.to_s
+            puts config.setup_method.to_s
+            puts config.setup_method.to_s
             zonenicstpzloginsetup(uii, opts) if state == 'setup' && config.setup_method == 'zlogin'
             zonenicdel(uii, opts) if state == 'delete'
           end
