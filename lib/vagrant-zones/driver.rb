3c0ae3aed713ca479b9426d8fb9c185bedeafc84
Mark <mark.gilbert@m4kr.net>
2022-01-02 16:44:55 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -392,6 +392,7 @@ module VagrantPlugins
         hvnic_name = "h_vnic_#{config.partition_id}_#{opts[:nic_number]}"
         vnic_configured = execute(false, "#{@pfexec} dladm show-vnic | grep #{hvnic_name} | awk '{ print $1 }' ")
         puts vnic_configured
+        puts hvnic_name
         uii.info(I18n.t('vagrant_zones.removing_vnic') + hvnic_name) if vnic_configured == hvnic_name.to_s
         execute(false, "#{@pfexec} dladm delete-vnic #{hvnic_name}") if vnic_configured == hvnic_name.to_s
         uii.info(I18n.t('vagrant_zones.no_removing_vnic')) unless vnic_configured == hvnic_name.to_s
