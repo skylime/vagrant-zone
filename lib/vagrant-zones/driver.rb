c5829b7fcb032c0573ccbe30efbbe202b1960b9d
Mark <mark.gilbert@m4kr.net>
2022-01-02 16:48:50 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -403,6 +403,8 @@ module VagrantPlugins
         vnic_name = vname(uii, opts)
         mac = macaddress(uii, opts)
         ether_configured = execute(false, "#{@pfexec} dladm show-etherstub | grep stub_#{vnic_name} | awk '{ print $1 }' ")
+        puts ether_configured
+        puts "#{@pfexec} dladm show-etherstub | grep stub_#{vnic_name} | awk '{ print $1 }' "
         uii.info(I18n.t('vagrant_zones.creating_ethervnic') + vnic_name.to_s) if ether_configured == "stub_#{vnic_name}"
         execute(false, "#{@pfexec} dladm delete-etherstub stub_#{vnic_name}") if ether_configured == "stub_#{vnic_name}"
       end
