e583e92ba29f3275827d62762b40e818af45b478
Mark <mark.gilbert@m4kr.net>
2022-01-03 03:58:07 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -105,6 +105,19 @@ module VagrantPlugins
         port = sshport(@machine).to_s
         port = 22 if sshport(@machine).to_s.nil?
         uii.info(I18n.t('vagrant_zones.ssh_run_command') + command) if config.debug
+
+        metrics ||= {}
+        metrics['instance_command_ssh_time'] = Util::Timer.time do
+          retryable(on: Errors::TimeoutError, tries: 60) do
+            # If we're interrupted don't worry about waiting
+ 
+            loop do
+              break if @machine.communicate.ready?
+            end
+          end
+        end
+        uii.info(I18n.t('vagrant_zones.boot_ready') + " in #{metrics['instance_dhcp_boot_time']} Seconds")
+
         execute(true, "#{@pfexec} pwd && ssh -o 'StrictHostKeyChecking=no' -p #{port} -i #{key} #{user}@#{ip} '#{command}' ")
         puts "#{@pfexec} pwd && ssh -o 'StrictHostKeyChecking=no' -p #{port} -i #{key} #{user}@#{ip} '#{command}' "
       end
