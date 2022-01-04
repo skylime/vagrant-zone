a9f6c6c2cf06f6e878f886c2693a329164449120
Mark <mark.gilbert@m4kr.net>
2022-01-04 08:23:20 -0600

    s

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -349,7 +349,7 @@ module VagrantPlugins
           end
         end       
         FileUtils.mv('/etc/dhcpd.conf-temp', '/etc/dhcpd.conf')
-        awk = %(| awk '{ $1=""; $2=""; sub(/^[ \t]+/, ""); print}' | tr ' ' '\n' | tr -d '"') 
+        awk = %(| awk '{ $1=""; $2=""; sub(/^[ \\t]+/, ""); print}' | tr ' ' '\\n' | tr -d '"') 
         cmd = %(svccfg -s dhcp:ipv4 listprop config/listen_ifnames )
         puts cmd + awk
         nicsused = execute(false, cmd.to_s + awk.to_s)
