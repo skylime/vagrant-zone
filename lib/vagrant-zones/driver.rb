be098cdd9398f41a57ff705bc2b3c11cfbd32b8f
Mark <mark.gilbert@m4kr.net>
2022-01-30 10:15:52 -0600

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -993,13 +993,14 @@ module VagrantPlugins
         alcheck = ' login:' if config.alcheck.nil?
         zlogin_write.printf("\n")
         def ratelimit?(n)
-          n % 10 == 0
+          n % 5 == 0
         end
         Timeout.timeout(config.setup_wait) do
           rsp = []
           n = 0
           loop do
             n += 1
+            puts n
             zlogin_write.printf("\n") if ratelimit?(n)
             zlogin_read.expect(/\r\n/) { |line| rsp.push line }
             uii.info(I18n.t('vagrant_zones.terminal_access_auto_login') + "'#{alcheck}'") if rsp[-1].to_s.match(/#{alcheck}/)
