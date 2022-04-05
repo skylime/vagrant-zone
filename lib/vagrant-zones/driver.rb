124d5d0c7df9c0a7c382794e12f9aa94dee381cc
Mark <mark.gilbert@m4kr.net>
2022-04-04 20:02:50 -0500

    save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1103,11 +1103,10 @@ module VagrantPlugins
             loop do
               zlogin_read.expect(/\r\n/) { |line| rsp.push line }
               uii.info(rsp[-1]) if config.debug_boot
-              sleep(5) if rsp[-1].to_s.match(/#{zunlockboot}/)
+              sleep(2) if rsp[-1].to_s.match(/#{zunlockboot}/)
               zlogin_write.printf("\n") if rsp[-1].to_s.match(/#{zunlockboot}/)
-              zlogin_write.printf(zunlockbootkey) if rsp[-1].to_s.match(/#{zunlockboot}/)
+              zlogin_write.printf("#{zunlockbootkey}\n") if rsp[-1].to_s.match(/#{zunlockboot}/)
               
-              zlogin_write.printf("\n") if rsp[-1].to_s.match(/#{zunlockboot}/)
               sleep(15) if rsp[-1].to_s.match(/#{bstring}/)
               zlogin_write.printf("\n") if rsp[-1].to_s.match(/#{bstring}/)
               break if rsp[-1].to_s.match(/#{bstring}/)
